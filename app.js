16cf9b1c25aae37c9ca63ae179c29000f618ed33
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-03-17 22:25:36 +0100

    made changes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import router from './router.js';
 
+dotenv.config();
+
 const app = express();
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(router);
-
-app.use(express.static('public'));
 
 app.listen(2207, () => {
   console.log('App is listening');
