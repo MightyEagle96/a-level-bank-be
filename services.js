00b493896361131f72ea210fb27a892222f2ab05
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2022-03-22 20:07:40 +0100

    made changes

--- services.js
+++ services.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 export const originUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://sarniarockinternational.web.app'
+    ? 'https://alevelbank.web.app'
     : 'http://localhost:3000';
